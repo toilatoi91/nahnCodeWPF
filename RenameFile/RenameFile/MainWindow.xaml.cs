0a096d491b34b3f70d16fd8564a525c0e7ebafd8
toilatoi91 <nhan.ph2609@sv.sgu.edu.vn>
2016-09-22 22:21:32 +0700

    init wpf with multi language

---
+++ RenameFile/RenameFile/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RenameFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AllVehiclesButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectCulture("vi-VN");
+            SetLanguageDictionary();
+        }
+        public static void SelectCulture(string culture)
+        {
+            if (String.IsNullOrEmpty(culture))
+                return;
+
+            //Copy all MergedDictionarys into a auxiliar list.
+            var dictionaryList = Application.Current.Resources.MergedDictionaries.ToList();
+
+            //Search for the specified culture.     
+            string requestedCulture = string.Format("StringResources.{0}.xaml", culture);
+            var resourceDictionary = dictionaryList.
+                FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+
+            if (resourceDictionary == null)
+            {
+                //If not found, select our default language.             
+                requestedCulture = "StringResources.xaml";
+                resourceDictionary = dictionaryList.
+                    FirstOrDefault(d => d.Source.OriginalString == requestedCulture);
+            }
+
+            //If we have the requested resource, remove it from the list and place at the end.     
+            //Then this language will be our string table to use.      
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+
+            //Inform the threads of the new culture.     
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+
+        }
+
+        private void SetLanguageDictionary()
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+            switch (Thread.CurrentThread.CurrentCulture.ToString())
+            {
+                case "en-US":
+                    dict.Source = new Uri("StringResources.xaml",
+                                  UriKind.Relative);
+                    break;
+                case "vi-VN":
+                    dict.Source = new Uri("StringResources.vi-VN.xaml",
+                                       UriKind.Relative);              
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dict);
+        }
+    }
+}
